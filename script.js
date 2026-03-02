dbdbc3d43e36c1ac847bca75b3092a22623898db
YannisUNP <141173494+YannisUNP@users.noreply.github.com>
2026-03-02 16:16:24 +0200

    Refactor country data handling and update DOM elements

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch(`https://restcountries.com/v3.1/name/Italy`);
 const button = document.getElementById("search-btn");
 const input = document.getElementById("country-input");
 const spinner = document.getElementById("loading-spinner");
 const error = document.getElementById("error-message");
-const info = document.getElementById("country-id");
+const info = document.getElementById("country-info");
+const border = document.getElementById("bordering-countries");
 
 button.addEventListener("click", function(){
     let countryName = input.value;
@@ -18,23 +18,46 @@ async function loadData(countryName){
     try{
         showSpinner();
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-        const country = await response.json();
+        const data = await response.json();
 
-        if (!country.ok) {
-            error.textContent = "Input an actual country pleaseee";
-        }else{
-            error.textContent = "";
-        }
-        console.log(country);
+        console.log(data[0]);
+        
+        const country = data[0];
+
         info.innerHTML = `
             <h2>${country.name.common}</h2>
             <p><strong>Capital:</strong> ${country.capital[0]}</p>
             <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
             <p><strong>Region:</strong> ${country.region}</p>
             <img src="${country.flags.svg}" alt="${country.name.common} flag">`;
-        console.log(country);
+
+        const borderData = country.borders;
+        if(!borderData){
+            border.innerHTML = "No bordering countries";
+        }
+
+        border.innerHTML = "<h3>Bordering Countries:<h3>";
+        borderData.forEach(async (code) => {
+            try {
+            const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+            const data = await response.json();
+            const borderCountry = data[0];
+
+            border.innerHTML += `
+                <img src="${borderCountry.flags.svg}" 
+                    alt="${borderCountry.name.common} flag"
+                    width="60">
+                <p>${borderCountry.name.common}</p>
+            `;
+
+            } catch (err) {
+                console.error(err);
+            }
+        });
+        
     }catch(e){
         error.textContent = "Input an actual country pleaseee";
+        error.classList.remove("hidden");
     } finally{
         hideSpinner();
     }
